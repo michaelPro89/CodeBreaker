d408b18b7588e336195b5419af1e8263fd47c146
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 13:46:51 +0100

    Update OutputWindow.java

--- Code Breaker (game)/src/codebreaker/OutputWindow.java
+++ Code Breaker (game)/src/codebreaker/OutputWindow.java
@@ -1,8 +1,12 @@
 package codebreaker;
 
 /**
- *
+ * Code breaker is a game where you have to quess the randomly generated code before you run out of lives.
+ * 
  * @author Michal Switala
+ * Copyright © 2021 belongs solely to Michal Switala.
+ *
+ * You can reach me out on : https://github.com/michaelPro89
  */
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
