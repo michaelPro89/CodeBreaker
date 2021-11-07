376ae30c9ff08c1525fb52487ed525ceec944f75
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 13:40:02 +0100

    Update GameLogic.java

--- Code Breaker (game)/src/codebreaker/GameLogic.java
+++ Code Breaker (game)/src/codebreaker/GameLogic.java
@@ -5,8 +5,10 @@ import java.awt.Desktop;
 import java.net.URL;
 
 /**
- *
+ * Code breaker is a game where you have to quess the randomly generated code before you run out of lives.
+ * 
  * @author Michal Switala
+ * Copyright © 2021 belongs solely to Michal Switala.
  */
 class GameLogic {
 
