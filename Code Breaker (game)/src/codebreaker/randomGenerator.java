4bded28ea88857babc3ab7ab52a916f99e3f280a
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 13:48:22 +0100

    Update randomGenerator.java

--- Code Breaker (game)/src/codebreaker/randomGenerator.java
+++ Code Breaker (game)/src/codebreaker/randomGenerator.java
@@ -5,8 +5,12 @@ package codebreaker;
 import java.security.SecureRandom;
 
 /**
- *
+ * Code breaker is a game where you have to quess the randomly generated code before you run out of lives.
+ * 
  * @author Michal Switala
+ * Copyright © 2021 belongs solely to Michal Switala.
+ *
+ * You can reach me out on : https://github.com/michaelPro89
  */
 class randomGenerator 
 {
